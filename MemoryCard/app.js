9d3b57920ab36513b4a1daf06b7b23a9f2b08548
justRunnz <ruigaspar03@hotmail.com>
2022-01-13 21:01:12 +0000

    💚Updates app.js
    💙add comment and remove useless code |
    🤍add timer functions [start, pause, reset]
    💜add win games count in local storage
    💛add notifications when game is finished , game ready to play

--- MemoryCard/app.js
+++ MemoryCard/app.js
@@ -1,18 +1,22 @@
 var cartesDoubleF = document.querySelectorAll(".double-face");
-let carte =         document.querySelectorAll(".carte");
-let arriere =       document.querySelectorAll(".arriere");
-var btnRset =       document.createElement("BUTTON");
+let carte = document.querySelectorAll(".carte");
+let arriere = document.querySelectorAll(".arriere");
+var btnRset = document.createElement("BUTTON");
 btnRset.setAttribute("id", "btn-rset")
-var btnStart =      document.createElement("button");
-var grille =        document.querySelector('.grille');
+var btnStart = document.createElement("button");
+var grille = document.querySelector('.grille');
+let content = document.querySelectorAll('.content');
 
 
 let carteRetourne = false;
 let pCarte, sCarte;
-let cardSelected =  false;
-let count =         0;
+let cardSelected = false;
 
+let count = 0;
 
+var clickedOnce = false;
+
+refreshCount(); // refresh the count of the games won
 
 // For loop to change cards image
 for (i = 0; i < arriere.length; i++) {
@@ -23,9 +27,10 @@ for (i = 0; i < arriere.length; i++) {
 
 // Button to restart the game
 btnRset.addEventListener('click', () => { // Ajoute un écouteur d'événement au bouton
-    notifications("The game is ready")
-    document.getElementById("btn-rset").remove();
-    grille.style.display = "grid";
+    reset(); // Reset timer 
+    document.getElementById("btn-rset").remove(); // Remove reset button 
+
+    notifications("Refreshing the game")
 
     carte.forEach(carte => {
         carte.childNodes[1].classList.remove('active'); // Retourne toute les cartes
@@ -33,8 +38,9 @@ btnRset.addEventListener('click', () => { // Ajoute un écouteur d'événement a
 
     setTimeout(() => {
         location.reload(); // Recharge la page 1 seconde après l'animation
-        // document.body.innerHTML = grille; // Create the game when restart button is clicked
-    }, 1000); 
+
+        notifications2("The game is ready")
+    }, 1500);
 });
 
 
@@ -43,22 +49,25 @@ carte.forEach(card => {
     card.addEventListener('click', toggleCards)
     let random = Math.floor(Math.random() * 12);
     for (let i = 0; i < carte.length; i++) {
-        card.style.order = random;
+        card.style.order = random; // set cards order ramdom
     }
 });
 
-
+grille.addEventListener('click', start, {
+    once: true //start the timer on the first card click
+});
 
 function toggleCards(card) {
-    if (cardSelected) return; 
-    if (this === pCarte) return;
 
-    card.target.parentNode.classList.add("active");
+    if (cardSelected) return;
+    if (this === pCarte) return;
+    card.target.parentNode.classList.add("active"); // add class active
+
 
     if (!carteRetourne) {
         carteRetourne = true;
         pCarte = this;
-        
+
         return;
     } else {
         // carteRetourne = false;
@@ -72,43 +81,33 @@ function toggleCards(card) {
 function correspondence() {
 
     if (pCarte.getAttribute("data-attr") === sCarte.getAttribute("data-attr")) {
-        count += 1; 
+        count += 1; // add 1 to the count variable
 
         if (count == 6) {
-            btnRset.innerHTML = "Restart";
-            // btnRset.style.backgroundColor = "rgba(100,255,0,1)";
-            // btnRset.style.fontSize = "20px";
-            // btnRset.style.padding = "15px 25px";
-            // btnRset.style.border = "0px";
-            // btnRset.style.borderRadius = "10px";
-            // btnRset.style.position = "relative";
-            // btnRset.style.left = "46%";
-            // btnRset.style.top = "47%";
-            document.body.appendChild(btnRset);
-            notifications("Crongratulations! You won the game 🥳 🎉")
+            btnRset.innerHTML = "Restart"; // set text in the reset button
+            document.body.appendChild(btnRset); // Add reset button to the page
+            notifications2("Crongratulations! You won the game 🥳 🎉")
+
+            pause(); // pause the countup timer
 
 
-            // grille.parentNode.removeChild(grille);
-            grille.style.display = "none";
 
-            console.log(count)
+            var gameCount = parseInt(localStorage.getItem("count")); 
+            localStorage.setItem("count", ++gameCount); // add 1 to the value of the local storage
+
         }
-
-        pCarte.removeEventListener("click", toggleCards)
+        // remove the event listener if the cards match
+        pCarte.removeEventListener("click", toggleCards) 
         sCarte.removeEventListener("click", toggleCards)
         clearBoard();
-        // console.log(count)
-        // console.log("match")
-
-
 
     } else {
         cardSelected = true;
-
         setTimeout(() => {
             cardSelected = false;
 
-            pCarte.childNodes[1].classList.remove("active");
+            // remove class if cards d'ont match
+            pCarte.childNodes[1].classList.remove("active"); 
             sCarte.childNodes[1].classList.remove("active");
             clearBoard();
         }, 1000)
@@ -123,26 +122,90 @@ function clearBoard() {
 
 
 
-
 function notifications(message) {
-
     //Initialize our notification pattern
-
     document.querySelector('#notification_container').innerHTML += `
         <div class="content">
             <p>${message}</p>
         </div>
     `;
-
     // Add a timer to supress our notifications after 3 seconds
-
-    setTimeout(function() {
+    setTimeout(function () {
         document.querySelector('#notification_container .content').remove();
     }, 3000);
+}
 
+function notifications2(message) {
+    //Initialize our notification pattern
+    document.querySelector('#notification_container').innerHTML += `
+        <div class="content2">
+            <p>${message}</p>
+        </div>
+    `;
+    // Add a timer to supress our notifications after 3 seconds
+    setTimeout(function () {
+        document.querySelector('#notification_container .content2').remove();
+    }, 3000);
 }
 
 
-// grille.style.display = "none";
-// btnRset.innerHTML = "Restart";
-// document.body.appendChild(btnRset);
+function refreshCount() {
+    document.getElementById('game-count').innerText = parseInt(localStorage.getItem("count"));
+}
+
+
+//***************************************************************************************************** */
+//*************************************** TIMER ******************************************************* */
+//***************************************************************************************************** */
+
+let minute = 0;
+let second = 0;
+let millisecond = 0;
+
+let cron;
+
+function timer() {
+    if ((millisecond += 1) == 100) {
+        millisecond = 0;
+        second++;
+    }
+    if (second == 60) {
+        second = 0;
+        minute++;
+    }
+
+    document.getElementById('minute').innerText = returnData(minute); 
+    document.getElementById('second').innerText = returnData(second);
+
+}
+
+function start() {
+    cron = setInterval(() => {
+        timer();
+    }, 10);
+    console.log("time start")
+}
+
+function pause() {
+    clearInterval(cron);
+    console.log("time pause")
+}
+
+function reset() {
+
+    minute = 0;
+    second = 0;
+
+    document.getElementById('minute').innerText = '00';
+    document.getElementById('second').innerText = '00';
+
+    clearInterval(cron);
+
+    console.log("time reset")
+}
+
+
+function returnData(input) {
+    return input > 9 ? input : `0${input}`
+}
+
